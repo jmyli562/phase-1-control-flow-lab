7e094b325bb6d369d3ee4392fda412f0a8091352
Jimmy Li <jmyli562@gmail.com>
2023-03-14 20:09:38 -0700

    finishes lab

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let message = '';
+  if(distance <= 400){
+    message = "This one is on me!";
+  }else if(distance >= 400 && distance <= 2000){
+    message = "That will be twenty bucks."
+  }else if(distance > 2000){
+    if(distance > 2500){
+      message = "No can do."
+    }else{
+      message = "I will gladly take your thirty bucks."
+    }
+  }
+
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let message = city == "NYC" ? "Ok, sounds good." : "No go.";
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  
+  switch(tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+      break;
+  }
+
+  return message;
 }
